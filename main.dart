d9b63fe7274b04a67bd03bc8976b8c6a50d98394
fajferek <74104097+fajferekpl@users.noreply.github.com>
2021-04-18 00:34:45 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-    MaterialApp(
-      home: Text('Hello World')
-    ),
+void main() {
+  runApp(MaterialApp(
+    home: Material(
+      color: Colors.blue,
+      child: Center(child: Text(
+        "Hello World",
+        style: TextStyle(fontSize: 34, color: Colors.white))))
+  ),
   );
 }
